ed494c4f83779ecbd6e38bfabf0fc760b38932a9
swaranjit banerjee <swaranjitbanerjee7@gmail.com>
2024-07-19 22:28:02 +0530

    comment added

--- script.js
+++ script.js
@@ -45,7 +45,8 @@ function updateDOM(data) {
 }
 
 async function fetchWeather(location) {
-    const url = `http://api.weatherapi.com/v1/current.json?key=6fc74cf82bc44773a8a171855241407&q=${location}&aqi=no`
+    const url = `http://api.weatherapi.com/v1/current.json?key=6fc74cf82bc44773a8a171855241407&q=${location}&aqi=no`;
+    //fetch -> inbuilt function to get response from http server
     const response = await fetch(url);
     if (response.status == 400) {
         alert("location is invalid");
